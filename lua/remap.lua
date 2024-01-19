b1741a9eed40d3c3fc5b2f87cdbef75e9f1d1f1e
Pandu Supriyono <mail@supriyono.nl>
2024-01-19 18:47:22 +0100

    add shortcut for netrw

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,18 +1,18 @@
 vim.g.mapleader = " "
 
+vim.keymap.set("n", "<leader><leader>", ":Explore<CR>")
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "\\", ":b#<CR>")
-
-vim.keymap.set("x", "<leader>p", "\"_dP")
 
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("v", "<leader>Y", "\"+Y")
 
-vim.keymap.set("n", "<C-_>", ':let @/ = ""<CR>')
+vim.keymap.set("n", "<Esc>", ':let @/ = ""<CR>')
 
 vim.keymap.set("n", "<C-c>", ":q<CR>")
 vim.keymap.set("n", "<C-h>", "<C-w>h")
