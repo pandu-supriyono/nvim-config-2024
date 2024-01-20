17429ae6771c14500d24044f3007905fb3ae0c07
Pandu Supriyono <mail@supriyono.nl>
2024-01-20 18:18:09 +0100

    simplify treesitter conf

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,111 +1,59 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  version = false, -- last release is way too old and doesn't work on Windows
-  build = ":TSUpdate",
-  init = function(plugin)
-    -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-    -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-    -- no longer trigger the **nvim-treeitter** module to be loaded in time.
-    -- Luckily, the only thins that those plugins need are the custom queries, which we make available
-    -- during startup.
-    require("lazy.core.loader").add_to_rtp(plugin)
-    require("nvim-treesitter.query_predicates")
-  end,
-  dependencies = {
-    {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      config = function()
-        -- When in diff mode, we want to use the default
-        -- vim text objects c & C instead of the treesitter ones.
-        local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
-        local configs = require("nvim-treesitter.configs")
-        for name, fn in pairs(move) do
-          if name:find("goto") == 1 then
-            move[name] = function(q, ...)
-              if vim.wo.diff then
-                local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
-                for key, query in pairs(config or {}) do
-                  if q == query and key:find("[%]%[][cC]") then
-                    vim.cmd("normal! " .. key)
-                    return
-                  end
-                end
-              end
-              return fn(q, ...)
-            end
-          end
-        end
-      end,
-    },
-  },
-  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-  keys = {
-    { "<c-space>", desc = "Increment selection" },
-    { "<bs>", desc = "Decrement selection", mode = "x" },
-  },
-  ---@type TSConfig
-  ---@diagnostic disable-next-line: missing-fields
-  opts = {
-    highlight = { enable = true },
-    indent = { enable = true },
-    ensure_installed = {
-      "bash",
-      "c",
-      "diff",
-      "go",
-      "html",
-      "javascript",
-      "jsdoc",
-      "json",
-      "jsonc",
-      "lua",
-      "luadoc",
-      "luap",
-      "markdown",
-      "markdown_inline",
-      "php",
-      "python",
-      "query",
-      "regex",
-      "toml",
-      "tsx",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "yaml",
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<C-space>",
-        node_incremental = "<C-space>",
-        scope_incremental = false,
-        node_decremental = "<bs>",
-      },
-    },
-    textobjects = {
-      move = {
-        enable = true,
-        goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-        goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-        goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-        goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
-      },
-    },
-  },
-  ---@param opts TSConfig
-  config = function(_, opts)
-    if type(opts.ensure_installed) == "table" then
-      ---@type table<string, boolean>
-      local added = {}
-      opts.ensure_installed = vim.tbl_filter(function(lang)
-        if added[lang] then
-          return false
-        end
-        added[lang] = true
-        return true
-      end, opts.ensure_installed)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function ()
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = {
+          "bash",
+          "c",
+          "diff",
+          "go",
+          "html",
+          "javascript",
+          "jsdoc",
+          "json",
+          "jsonc",
+          "lua",
+          "luadoc",
+          "luap",
+          "markdown",
+          "markdown_inline",
+          "python",
+          "query",
+          "regex",
+          "toml",
+          "tsx",
+          "typescript",
+          "vim",
+          "vimdoc",
+          "yaml",
+        },
+        highlight = { enable = true },
+        indent = {
+              enable = true,
+              disable = { "python" }
+          }, -- disable python indent is needed for python indentation to work
+        incremental_selection = {
+          enable = true,
+        },
+      }
     end
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+      require("treesitter-context").setup{
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        line_numbers = true,
+        multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
+        trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = "cursor",  -- Line used to calculate context. Choices: 'cursor', 'topline'
+        separator = nil,
+        zindex = 20, -- The Z-index of the context window
+        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+      }
+    end
+  }
 }
