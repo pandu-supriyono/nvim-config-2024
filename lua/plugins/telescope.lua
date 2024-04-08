d0094d01d3eb87c356e75c60920572fbca66a296
Pandu Supriyono <mail@supriyono.nl>
2024-04-08 09:10:34 +0200

    add search references telescope shortcut

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,5 +8,6 @@ return {
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
     vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+    vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {})
   end
 }
