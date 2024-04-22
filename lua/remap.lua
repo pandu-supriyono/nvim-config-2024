5dcc03ce8de8c3d03330421886f1a7c647cc275e
Pandu Supriyono <mail@supriyono.nl>
2024-04-22 10:38:09 +0200

    remap alternate key to backspace

--- lua/remap.lua
+++ lua/remap.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = " "
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "\\", ":b#<CR>")
+vim.keymap.set("n", "<BS>", ":b#<CR>")
 
 vim.keymap.set("n", "<leader>bd", ":bd<CR>")
 
